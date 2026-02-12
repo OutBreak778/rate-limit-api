b00b0cf229b0ba8b27d076f4e83301fd2cf8fa00
Aman Mishra <aman429119@gmail.com>
2026-02-12 14:38:34 +0530

    added cors properly

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ import subscriptionRouter from "./routes/subscription-route.js"
 import cookieParser from "cookie-parser"
 import { errorMiddleware } from "./middlewares/error.js"
 import taskRouter from "./routes/task-route.js"
+import cors from "cors"
 
 const app = express()
 dotenv.config({
     path: '.env'
 })
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
